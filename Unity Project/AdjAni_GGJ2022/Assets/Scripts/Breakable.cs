77d91428cfb6923a2c98df95d2165d8ad6a58b93
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2022-01-28 19:13:53 +0100

    Add base breakable script

---
+++ Unity Project/AdjAni_GGJ2022/Assets/Scripts/Breakable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public abstract class Breakable : MonoBehaviour
+{
+    bool itemBroken = false;
+    bool itemFixed = false;
+
+    public void Break()
+    {
+        if (!itemBroken && !itemFixed)
+        {
+            HandleBreak();
+            itemBroken = true;
+        }
+    }
+    public void Fix()
+    {
+        if(itemBroken && !itemFixed)
+        {
+            HandleFix();
+            itemFixed = true;
+        }
+    }
+
+    public abstract void HandleBreak();
+    public abstract void HandleFix();
+}
