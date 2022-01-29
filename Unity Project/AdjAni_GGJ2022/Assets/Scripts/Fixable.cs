69aa591f931343a074f034f22019e3758efa0cfa
Unknown <pepijn.thijssens@gmail.com>
2022-01-29 17:12:23 +0100

    Import particles, add particle system, add pS to prefabs

--- Unity Project/AdjAni_GGJ2022/Assets/Scripts/Fixable.cs
+++ Unity Project/AdjAni_GGJ2022/Assets/Scripts/Fixable.cs
@@ -11,6 +11,7 @@ public class Fixable : MonoBehaviour
     [SerializeField] private int requiredInteractions = 5;
     [SerializeField] private int currentInteractions = 0;
 
+    [SerializeField] private ParticleSystem PS;
     
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,12 @@ public class Fixable : MonoBehaviour
         if (Input.GetKeyDown(input))
         {
             GameController.Instance.CamShakeEffect.Shake();
+
+            if(PS != null)
+            {
+                PS.Stop();
+                PS.Play();
+            }
             transform.DOShakeRotation(0.3f, new Vector3(0, 0, 1), 5,10);
             transform.DOPunchScale(new Vector3((float)transform.localScale.x+0.1f, (float)transform.localScale.y + 0.1f, 1), 0.3f, 10, 1);
             currentInteractions++;
