45d29ba1a74f7967ccf8297670cd6613c4a30156
Unknown <pepijn.thijssens@gmail.com>
2022-01-30 00:49:19 +0100

    Fix highscore text resizing (kinda maybe)

--- Unity Project/AdjAni_GGJ2022/Assets/Scripts/HighScore/DisplayHighscores.cs
+++ Unity Project/AdjAni_GGJ2022/Assets/Scripts/HighScore/DisplayHighscores.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+using TMPro;
 
 public class DisplayHighscores : MonoBehaviour 
 {
@@ -29,7 +31,16 @@ public class DisplayHighscores : MonoBehaviour
                 rNames[i].text = highscoreList[i].username;
             }
         }
+
+        Debug.Log("Resizing Text Together..");
+        ResizeAllTextFonts();
     }
+
+    private void ResizeAllTextFonts()
+    {
+        AutoResizeTextTogether(rNames.Concat(rScores).ToList());
+    }
+
     IEnumerator RefreshHighscores() //Refreshes the scores every 30 seconds
     {
         while(true)
@@ -37,5 +48,41 @@ public class DisplayHighscores : MonoBehaviour
             myScores.DownloadScores();
             yield return new WaitForSeconds(30);
         }
+    }
+
+    //rip that shit
+    private void AutoResizeTextTogether(List<TextMeshProUGUI> TextObjects)
+    {
+        if (TextObjects == null || TextObjects.Count == 0)
+            return;
+
+        Debug.Log("Resizing Text Together.. in functions");
+        // Iterate over each of the text objects in the array to find a good test candidate
+        // There are different ways to figure out the best candidate
+        // Preferred width works fine for single line text objects
+        int candidateIndex = 0;
+        float maxPreferredWidth = 0;
+
+        for (int i = 0; i < TextObjects.Count; i++)
+        {
+            float preferredWidth = TextObjects[i].preferredWidth;
+            if (preferredWidth > maxPreferredWidth)
+            {
+                maxPreferredWidth = preferredWidth;
+                candidateIndex = i;
+            }
+        }
+
+        // Force an update of the candidate text object so we can retrieve its optimum point size.
+        TextObjects[candidateIndex].enableAutoSizing = true;
+        TextObjects[candidateIndex].ForceMeshUpdate();
+        float optimumPointSize = TextObjects[candidateIndex].fontSize;
+
+        // Disable auto size on our test candidate
+        TextObjects[candidateIndex].enableAutoSizing = false;
+
+        // Iterate over all other text objects to set the point size
+        for (int i = 0; i < TextObjects.Count; i++)
+            TextObjects[i].fontSize = optimumPointSize;
     }
 }
