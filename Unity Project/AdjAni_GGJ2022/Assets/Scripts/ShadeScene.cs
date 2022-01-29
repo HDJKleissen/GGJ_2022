3b34f5bac24655b1e208b25303c63ae6c82764e5
Unknown <pepijn.thijssens@gmail.com>
2022-01-29 03:03:31 +0100

    Add greyscale shader, controllerscript that flips colors

--- Unity Project/AdjAni_GGJ2022/Assets/Scripts/ShadeScene.cs
+++ Unity Project/AdjAni_GGJ2022/Assets/Scripts/ShadeScene.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using System;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ShadeScene : MonoBehaviour
 {
+    bool dogScene = true;
+    [SerializeField] private List<SpriteRenderer> objects;
     // Start is called before the first frame update
     void Start()
     {
-        
+        objects = GetAllObjectsOnlyInScene();
+        SetGreyScale(objects, true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    List<SpriteRenderer> GetAllObjectsOnlyInScene()
+    {
+        List<SpriteRenderer> SRInScene = new List<SpriteRenderer>();
+
+        foreach (SpriteRenderer SR in Resources.FindObjectsOfTypeAll(typeof(SpriteRenderer)) as SpriteRenderer[])
+        {
+            if (!EditorUtility.IsPersistent(SR.gameObject.transform.root.gameObject) && !(SR.gameObject.hideFlags == HideFlags.NotEditable || SR.gameObject.hideFlags == HideFlags.HideAndDontSave))
+                SRInScene.Add(SR);
+        }
+
+        return SRInScene;
+    }
+
+    void SetGreyScale(List<SpriteRenderer> renderers, bool on)
+    {
+        foreach(SpriteRenderer r in renderers)
+        {
+            r.material.SetInt("_IsGreyScale", Convert.ToInt32(on));
+        }
     }
 }
